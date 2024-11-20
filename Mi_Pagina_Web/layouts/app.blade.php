e5c931eee38b088a84bcbb0ae652b4a99bfbf674
Alexx-MS <jhoniersamboni2504@gmail.com>
2024-11-20 11:49:45 -0500

    clase de html miercoles

---
+++ Mi_Pagina_Web/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+<body>
+    <header>
+            <h1>Bienvenidos a Mi Página Web</h1>
+            <nav>
+                <ul>
+                    <li><a href="#index.html">Inicio</a></li>
+                    <li><a href="about.html">Acerca de</a></li>
+                    <li><a href="#contact.html">Contacto</a></li>
+                </ul>
+            </nav>
+    </header>
+
+
+    @yield('section')
+    <div>
+    
+    </div>
+    
+    <footer>
+        <p>&copy; 2024 Mi Página Web. Todos los derechos reservados.</p>
+    </footer>
+
+</html>
