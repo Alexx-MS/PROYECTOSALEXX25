db342dc0b0cf8bbe3bd124024c9801c8ae88fdc1
Alexx-MS <jhoniersamboni2504@gmail.com>
2024-11-21 15:39:18 -0500

    version16.1
    Taller de JavaScript

---
+++ JavaScript/scripts/JavaScript.js
@@ -0,0 +1,12 @@
+// const boton = document.querySelector("button");
+// boton.textContent = "Texto cambiado : El bicho siuu CR7";
+
+// const boton = document.querySelector('#elboton');
+// boton.addEventListener('click', () => {
+// boton.style.backgroundColor = 'blue';
+// });
+
+const boton = document.querySelector('#elboton');
+boton.addEventListener('click', () => {
+  alert('Estas clickanding este boton 😂😂')
+});
